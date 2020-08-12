87226b0dbaa06b1e57320c0f8071d494e945f1c2
ShokurovAndrey <react-racing@yandex.ru>
2020-08-12 18:57:09 +0400

    commit two

--- main.py
+++
@@ -1,9 +0,0 @@
-age = 27
-name = 'Andrey.'
-print('My name is -', name, 'I`m', age, 'years old')
-age = 27
-name = 'Andrey.'
-print('My name is -', name, 'I`m', age, 'years old')
-age = 27
-name = 'Andrey.'
-print('My name is -', name, 'I`m', age, 'years old')
