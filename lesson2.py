a61531d7bbe1671ae67f9a71e513af484f64b296
ShokurovAndrey <react-racing@yandex.ru>
2020-08-12 21:15:54 +0400

    commit les

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,84 @@
-x = 5 > 10
+# x = 5 < 10
+# print(x, type(x))
+
+# x = False
+# if x == 1:
+#     print('Yes')
+# elif x:
+#     print('Noway')
+# else:
+#     print('No')
+#
+# print(x)
+
+# original_password = '777'
+# password = input('Enter password: ')
+# access = 0
+# if password == original_password:
+#     print('Welcome')
+#     access = 1
+# else:
+#     print('Bye bye')
+
+# color = str(input('Введите цвет: '))
+# if color == 'blue':
+#     print('синий')
+# elif color == 'red':
+#     print('красный')
+# elif color == 'green':
+#     print('зеленый')
+# else:
+#     print('цвет неизвестный')
+
+# a = 0
+# while a < 7:
+#     print('A')
+#     a += 1
+
+# vv = str(input('Say Y/N: '))
+# while vv == 'y':
+#     vv = str(input('Say Y/N: '))
+# print('Yeeees')
+
+# a = 0
+# while a >= 0:
+#     if a == 7:
+#         break
+#     a += 1
+#     print('A --', a)
+
+# a = -1
+# while a < 10:
+#     a += 1
+#     if a == 7 or a % 2 == 1:  # VSE CHETNII
+#         continue
+#     print('B --', a)
+
+# бесконечный цикл
+# a = 5
+# while a>0:
+#     print('!')
+#     a += 1
+
+# i = 0
+# while True:
+#     i += 1
+#     if i >= 10:
+#         break
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+#     i += 1
+
+# for i in range(5):
+#     print('GG')
+
+# word_str = 'Hello, world'
+# for l in word_str:
+#     print(l)
+#
+#
+# lst = [1, 2, 3, 5, 7, 9]
+# for i in lst:
+#     print(i ** 2)
+
