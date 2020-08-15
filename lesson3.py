6d94ea329c964ac66b3290755039cc862e12be0d
ShokurovAndrey <react-racing@yandex.ru>
2020-08-15 20:19:23 +0400

    commit

--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,15 @@
-num = input('enter number: ')
-e = 0
 
-if digit in str(num):
-    e = digit
+num = input('+/-: ')
+i = 0
+if num == '+':
+    while i < 21:
+        if i % 2 == 0:
+            print(i, end=' ')
+        i += 1
+elif num == '-':
+    while i < 21:
+        if i % 2 == 1:
+            print(i, end=' ')
+        i += 1
+else:
+    print('Я не понимаю, что вы от меня хотите...')
