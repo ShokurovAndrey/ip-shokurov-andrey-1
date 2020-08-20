d6c2d19409961620859422483c092cedef101b3e
ShokurovAndrey <react-racing@yandex.ru>
2020-08-20 18:11:16 +0400

    commit

--- lesson 4.py
+++ lesson 4.py
@@ -1,125 +1,141 @@
-# for i in range(0, 8, 2): # 0 - начало, 8 - конец, 2 - шаг
-#     print(i, end=" ")
+for i in range(0, 8, 2):  # 0 - начало, 8 - конец, 2 - шаг
+    print(i, end=" ")
 
-# print(abs(-2))  # вывод абсолютной велечины
+print(abs(-2))  # вывод абсолютной велечины
 
-# print(max([1,2,3,4,5])) # максимальное значение
-# x = round(5.876) # округление до большего
-# x = sum([2.56, 1, 12]) # суммирует все элементы последовательности
-# x = type('') # возвращаяет тип объекта
-# x = [1,2,3,4,5]
-# for i, y in enumerate(x): # enumerate возвращ. пару (индекс, значение)
-#     print(i, y)
-
-# def summ(a, b):
-#     c = a + b
-#     return c
-#
-#
-# def test():
-#     """
-#     ничего
-#     :return:
-#     """
-#     pass
-#
-#
-# x = 5
-# y = 6
-# s = summ(x, y)
-# print(s)
-# print(test())
-
-# f = lambda x: x ** 2
-# print(f(4))
-# print((lambda x: x ** 2)(4))
-
-# x = 5
-# def outside():
-#     y = 10
-#     def inside():
-#         z = 15
-#         print('inside x: {}, y: {}, z: {}'.format(x, y, z))
-#     inside()
-#     print('outside x: {}, y: {}, z: {}'.format(x, y, 'z - недоступна'))
-# outside()
-# print('inside x: {}, y: {}, z: {}'.format(x, 'y, z - недоступна'))
-
-# x = 5
-# def wrapper():
-#     def test1():
-#         x = 10
-#         print('test1 x = ', x)
-#     def test2():
-#         print('test2 x = ', x)
-#     def test3():
-#         global x
-#         print('test3 x = ', x)
-#         x = 25
-#     test1()
-#     test2()
-#     test3()
-# wrapper()
-# print('after wrapper x = ', x)
-
-# def average(*args):
-#     summ = 0
-#     for arg in args:
-#         summ += arg
-#     return summ / len(args)
-#
-#
-# print(average(1, 2, 3))
-#
-#
-# def format(**kwargs):
-#     print('You name is %s %s. Your age is %s. And your address is: %s' %
-#           (kwargs['name'], kwargs['surname'], kwargs['age'], kwargs['address']))
-#
-#
-# format(name='Vasa', surname='Ivaniv', age='12', address='Poop.str 22')
+print(max([1, 2, 3, 4, 5]))  # максимальное значение
+x = round(5.876)  # округление до большего
+x1 = sum([2.56, 1, 12])  # суммирует все элементы последовательности
+x2 = type('')  # возвращаяет тип объекта
+x3 = [1, 2, 3, 4, 5]
+for i, y in enumerate(x):  # enumerate возвращ. пару (индекс, значение)
+    print(i, y)
 
 
-# def welcome(name='Pool'):
-#     print('Welcome, %s' % name)
-#
-#
-# def prin(*args, sep='', end=''):
-#     print('Иван,Иванович,иванов', sep='//', end='!!!')
-#
-#
-# print(welcome('user'))
-# print(welcome())
+def summ(a, b):
+    c = a + b
+    return c
 
 
-# a = [1, 2, 4]
-# b = [3, 4]
-# c = [5, 6, 0]
-# for i in zip(a, b, c): # зип - итератор который генерирует серию множеств, содержащие элементы из каждой итерации
-#     print(i)
-# print(zip(a, b, c))
+def test():
+    """
+    ничего
+    :return:
+    """
+    pass
 
-# print(list(map(lambda x: x * x, [2, 5, 12, -2]))) # map приеняется последовательно и возвращается в виде итератора
+
+x = 5
+y = 6
+s = summ(x, y)
+print(s)
+print(test())
+
+f = lambda x: x ** 2
+print(f(4))
+print((lambda x: x ** 2)(4))
+
+x = 5
+
+
+def outside():
+    y = 10
+
+    def inside():
+        z = 15
+        print('inside x: {}, y: {}, z: {}'.format(x, y, z))
+
+    inside()
+    print('outside x: {}, y: {}, z: {}'.format(x, y, 'z - недоступна'))
+
+
+outside()
+print('inside x: {}, y: {}, z: {}'.format(x, 'y, z - недоступна'))
+
+x = 5
+
+
+def wrapper():
+    def test1():
+        x = 10
+        print('test1 x = ', x)
+
+    def test2():
+        print('test2 x = ', x)
+
+    def test3():
+        global x
+        print('test3 x = ', x)
+        x = 25
+
+    test1()
+    test2()
+    test3()
+
+
+wrapper()
+print('after wrapper x = ', x)
+
+
+def average(*args):
+    summ = 0
+    for arg in args:
+        summ += arg
+    return summ / len(args)
+
+
+print(average(1, 2, 3))
+
+
+def format(**kwargs):
+    print('You name is %s %s. Your age is %s. And your address is: %s' %
+          (kwargs['name'], kwargs['surname'], kwargs['age'], kwargs['address']))
+
+
+format(name='Vasa', surname='Ivaniv', age='12', address='Poop.str 22')
+
+
+def welcome(name='Pool'):
+    print('Welcome, %s' % name)
+
+
+def prin(*args, sep='', end=''):
+    print('Иван,Иванович,иванов', sep='//', end='!!!')
+
+
+print(welcome('user'))
+print(welcome())
+
+a = [1, 2, 4]
+b = [3, 4]
+c = [5, 6, 0]
+for i in zip(a, b, c):  # зип - итератор который генерирует серию множеств, содержащие элементы из каждой итерации
+    print(i)
+print(zip(a, b, c))
+
+print(list(map(lambda x: x * x, [2, 5, 12, -2])))  # map приеняется последовательно и возвращается в виде итератора
 
 # filter - итератор с отфильтрованием элементов функцией filter_func
-# print(list(filter(lambda x: x > 5, [2, 10, -10, 8, 2, 0, 14])))
-# # отбрасываем все элементы с длиной НОЛЬ
-# print(list(filter(len, ['', 'not null', 'bla','', '10'])))
-
-# import os
-# path = os.path.join('lesson1.py')  # хороший кроссплатформенный метод указания пути
-# f = open(path, 'r', encoding='UTF-8')
-# print(f.readlines())  # считываем все из файла
-# f.close()
+print(list(filter(lambda x: x > 5, [2, 10, -10, 8, 2, 0, 14])))
+# отбрасываем все элементы с длиной НОЛЬ
+print(list(filter(len, ['', 'not null', 'bla', '', '10'])))
 
 import os
-path = os.path.join ('lesson1.py')
+
+path = os.path.join('lesson1.py')  # хороший кроссплатформенный метод указания пути
+f = open(path, 'r', encoding='UTF-8')
+print(f.readlines())  # считываем все из файла
+f.close()
+
+import os
+
+path = os.path.join('lesson1.py')
 f = open(path, 'r', encoding='UTF-8')
 wanted_symbol = '+'
 for line in f:  # считываем файл построчно
-    if wanted_symbol in line:   # пока не найдем нужную информацию
+    if wanted_symbol in line:  # пока не найдем нужную информацию
         print(line)
-        break   # как нашли, заканчиваем чтение файла
+        break  # как нашли, заканчиваем чтение файла
 # наиболее правильный способ работы с файлами
 # по окончанию with, гарантированно закроется фалй
 with open(path, 'r', encoding='UTF-8') as f:
--- lesson3.py
+++ lesson3.py
@@ -1,176 +1,37 @@
-# 1 операции со строками
+# Задача-1:
+# Дан список фруктов.
+# Напишите программу, выводящую фрукты в виде нумерованного списка,
+# выровненного по правой стороне.
 
-str = 'hello'
-str_1 = 'lololololololotttololo'
-a = 'Start'
-b = 'Stop'
-c = 'Step'
-# str[a:b:c] индексация начинается с нуля
-# .title(), .upper(), .lower(), find(), and etc.
+# Пример:
+# Дано: ["яблоко", "банан", "киви", "арбуз"]
+# Вывод:
+# 1. яблоко
+# 2.  банан
+# 3.   киви
+# 4.  арбуз
 
-# metod string
-# print(str_1.find('ttt'), len(str_1))
-#
-# # format str
-# name = 'Eric'
-# surname = 'Smith'
-# print('Welcome' + name + '' + surname + '!')
-# # old ver format
-# print('Welcome %s %s !' % (name, surname))
-# # py.3.6
-# print('Welcome {} {} !'.format(name,surname))
-# # new ver format
-# print('Welcome {1} {0} !'.format(name, surname))
-# # форматирование строк
-# print(f'welcome {name} {surname} !')
-#
-#
-# # списки
-#
-# empty_list = []
-# my_list = [1, 3, 5, 3.45, 'ddd', 's', 333]
-# print('my_list = ', my_list)
+# Подсказка: воспользоваться методом .format()
 
-# print(my_list[0])
-# print(my_list[1])
-# срезы
-# print(my_list[0:-3])  # 3 последних элемента списка
-# конкантенация
-# print(my_list[0:3] + [7, 8, 9])
-# print(my_list * 3)
-# print(my_list[1] * 3)
-# my_list[4] = 2
-# print(my_list)
+lavka_ashota = ['apple', 'banana', 'mango']  # :D
+i = 0
+while len(lavka_ashota) > i:
+    print(i + 1, '{:>10}'.format(lavka_ashota[i]))
+    i += 1
+print()
 
-# my_list = [1, [1, [3]]]
-# print(my_list[1][1][0])
-#
-# my_list.append(['1', 3.2])
-# print(my_list)
-# my_list[1][1].append(['1', 3.2])
-# print(my_list)
-#
-#
-# lst = [1, 2, 3]
-# lst.append(4) # добавить в конец списка
-# lst.pop() # Удалить последний элемент
-# lst.pop(1) # Удалить элемент с индексом 1
-# delet
-# x = [1,2,3]
-# print(x)
-# print(x.pop())
-# print(x)
-# del x[0]
-# print(x)
-#
-#
-# # Кортежи
-#
-# t = ()
-# t11 = (2,)
-# t1 = 2,
-# print(t, t11, t1)
+for fruit in lavka_ashota:
+    i += 1
+    print(i, '{:>10}'.format(fruit))
+print()
 
-# for in  для последовательностей
-# fruits = ['apple', 'banana', 'mango']
-# i = 0
-# while len(fruits) > i:
-#     print('fruit = ', fruits[i])
-#     i += 1
-#
-# # # # # # # # # # # # # # # # # # # # # #
-# fruits = ['apple', 'banana', 'mango']
-# for fruit in fruits:
-#     print('fruit = ', fruit)
-#
-# # # # # # # # # # # # # # # # # # # # #
-# for el in 'Hello':
-#     print('el = ', el)
-# print()
-# for t_el in 1,2,3,4,5,10:
-#     print('--------')
-#     print('t_el = ', t_el)
-# print()
+# Задача-2:
+# Даны два произвольные списка.
+# Удалите из первого списка элементы, присутствующие во втором списке.
 
-# СЛОВАРИ
-
-# x = {'x', 'y', 'a'}
-# # # { key : znachenie}
-# print(x)
-# x['y'] = '1'
-# print(x)
-# x['y'] = {'1': 1}
-# print(x)
-
-# metod slovarey
-# cikl po slovar
-# for key, value in f.items():
-#     print(key, value)
-# for key in f.keys():
-#     print(key)
-# for value in f.values():
-#     print(value)
-# # удаляет элемент С и возвращает его значение
-# print(f.pop('c'))
-# print(f)
-# # удаляет и возвращает произвольную пару (ключ, значение)
-# print(f.popitem)
-#
-# print(x.items()) # возвращает пары ключ-значение
-# print(x.keys()) # возвращает список ключей
-# print(x.values()) # возвращает список значения
-# print(x.pop('y')) #
-# print(x.popitem()) #
-# print(x)
-# x = {'y': [], 'z': 1}
-# print(x)
-# x.pop('y')
-# print(x)
-#
-# x['y'] = 1
-# print(x)
-
-# a = set()
-# print('a = ', a) # set()
-# b = set(['a', 'b', 'c', 'c', 'a', 'e'])
-# print('b = ', b)
-# c = set('hello')
-# print('c = ', c)
-# d = {'a', 'b', 'c', 'd'}
-# print('d = ', d)
-# f = {} #  так получается словарь
-# print('type({}) ---> ', type(f)) # <class 'dict'>
-# # Операции с множествами
-# # print(len(e))
-# print("'b' in b ---> ", 'b' in b)
-# # s == t
-# c1 = {'e', 'l', 'o', 'h'}
-# print(c == c1)
-# # s.issubset(t)   s <= t
-# print(c <= c1)
-# # s.issuperset(t)s >=  t
-# print(c >= {'h'})
-# # s.union(t,...) s | t
-# print(b | d)
-# # s.intersection(t, ...) s & t
-# print(b & d)
-# # s.difference(t, ...) s - t
-# print(d - b)
-# # s.symmetric_difference(t) s^t
-# print(d^b)
-
-# a = set(['a', '3', '5'])
-# b = set(['a', '3'])
-# print(a)
-# print(b.issuperset(a))
-# myset = {1, 2, 3}
-# print(myset.issuperset([1, 2]))
-# print(myset.issuperset([1, 2, 3]))
-# print(myset.issuperset([1, 2, 3, 4]))
-
-d = dict.fromkeys(['a', 'b'])
-print(d)
-d['a'] = 'aa'
-print(d)
-d.update({'c': ['1'], 'b': 1})
-print(d)
+s_a = ['23', '0', '11', '-20', '100', '999']
+s_b = ['Summer', 'TNT', '11', 'xxx', 'qwerty', '999']
+s_a.pop(2)
+s_a.pop(5)
+print(s_a)
+print(s_b)
